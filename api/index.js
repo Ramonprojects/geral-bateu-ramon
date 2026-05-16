4828dbad5b0f747dd9625fd4c98731fd7ccbbeff
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-16 13:25:05 -0300

    chore: update shareCode to WJ7EC34PN2Z

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/iqnfrbwl?shareCode=NF4J2KXLUFC&afp=ot&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/iqnfrbwl?shareCode=WJ7EC34PN2Z&afp=ot&home=1' });
     res.end();
   }
