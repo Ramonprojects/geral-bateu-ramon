8bea963a1b3e083f0ad2af674ccd30499eb0a701
Ramonprojects <adm@ramonmago.com>
2026-06-15 10:32:48 -0300

    chore: shareCode TMDQEZF1LKY -> C3FDKUF0IGS (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=TMDQEZF1LKY&afp2=ot&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=C3FDKUF0IGS&afp2=ot&home=1' });
     res.end();
   }
