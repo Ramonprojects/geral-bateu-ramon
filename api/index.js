dfa7e8adfa47f931edafcc5874f0500fa9ad823b
Ramonprojects <adm@ramonmago.com>
2026-06-20 13:43:54 -0300

    chore: shareCode T0LIH2QSIQJ -> F98V12PZ32P (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=T0LIH2QSIQJ&afp2=ot&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=F98V12PZ32P&afp2=ot&home=1' });
     res.end();
   }
