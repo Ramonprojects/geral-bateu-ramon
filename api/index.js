b9594c6405a90fb2b75f249f62764c5cb888ea95
Ramonprojects <adm@ramonmago.com>
2026-06-24 13:31:48 -0300

    chore: shareCode NYN1Z6PXT4E -> PMP0FXWXKX9 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=NYN1Z6PXT4E&afp2=ot&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=PMP0FXWXKX9&afp2=ot&home=1' });
     res.end();
   }
