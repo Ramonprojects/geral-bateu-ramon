e74fd98e82a1ae2a8c764e83e0989844b01b4add
Ramonprojects <adm@ramonmago.com>
2026-06-27 10:32:47 -0300

    chore: shareCode XVU4BLECAJU -> 6W1S4NMKL4T (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=XVU4BLECAJU&afp2=ot&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=6W1S4NMKL4T&afp2=ot&home=1' });
     res.end();
   }
