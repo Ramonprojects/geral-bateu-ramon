9eaebaa753142cc5bd671bcfeec6a0486800a949
Ramonprojects <adm@ramonmago.com>
2026-07-03 10:06:40 -0300

    chore: shareCode VNPSQYMBOM4 -> WI62K29QYY4 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=VNPSQYMBOM4&afp2=ot&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=WI62K29QYY4&afp2=ot&home=1' });
     res.end();
   }
