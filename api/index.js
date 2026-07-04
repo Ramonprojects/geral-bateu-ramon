dc55a35e081fddec2e084fa1c7dd78ea748a9a9c
Ramonprojects <adm@ramonmago.com>
2026-07-04 09:45:42 -0300

    chore: shareCode WI62K29QYY4 -> M4MEV095HGD (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=WI62K29QYY4&afp2=ot&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=M4MEV095HGD&afp2=ot&home=1' });
     res.end();
   }
